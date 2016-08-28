4d0a7bf25150acbf4d660190109a3614ef555b62
BLACKJALS <jholeon@uan.edu.co>
2016-08-27 20:25:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alejandro/foodmap/Inicio.java
@@ -0,0 +1,30 @@
+package com.example.alejandro.foodmap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+    Button botonAcercaDe;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        botonAcercaDe = (Button)findViewById(R.id.button2);
+
+        botonAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Inicio.this, AcercaDe.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/alejandro/foodmap/Splash_Screen.java
@@ -0,0 +1,41 @@
+package com.example.alejandro.foodmap;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Splash_Screen extends AppCompatActivity {
+    private static int SPLASH_TIEMPO = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        new Handler().postDelayed(new Runnable() {
+
+			/*
+			* Mostramos la pantalla de bienvenida con un temporizador.
+			* De esta forma se puede mostrar el logo de la app o
+			* compañia durante unos segundos.
+			*/
+
+            @Override
+            public void run() {
+                // Este método se ejecuta cuando se consume el tiempo del temporizador.
+                // Se pasa a la activity principal
+                Intent i = new Intent(Splash_Screen.this,  Inicio.class);
+                startActivity(i);
+                // Cerramos esta activity
+                finish();
+            }
+        }, SPLASH_TIEMPO);
+    }
+}
